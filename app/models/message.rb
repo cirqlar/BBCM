67eb5115b2b9a7620d9b5353d5a09f66fd956c57
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@yahoo.com>
2016-07-04 16:56:33 +0100

    Add tagging to Messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Base
   mount_uploader :poster, ImageUploader
   mount_uploader :media, MediaUploader
+  acts_as_taggable
   validates :title, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { maximum: 250 }
   validates :media, presence: true
