184dd6ff54950e56b1e6721b408179be29bf37a3
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@yahoo.com>
2016-07-13 20:50:01 +0100

    Add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,14 @@ module ApplicationHelper
     action == params[:action] ? true : false
   end
 
+  def title_er
+    unless controller?('pages')
+      return params[:controller].capitalize + " | "
+    end
+    return params[:action].capitalize + " | " unless action?('index')
+    return 'Home | '
+  end
+
   def nester(nest)
     nest.map do |branch, leaves|
       render(branch) + content_tag(:div, nester(leaves), class: "children")
