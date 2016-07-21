5f86023e50aee2e3c67c08d4c9d051c099f4119e
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2016-07-21 07:30:22 +0100

    Undo a lot

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,14 +2,7 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://localhost:6379/0' }
-  database_url = '//localhost:6379/0'
-  if database_url
-    ActiveRecord::Base.establish_connection("#{database_url}?pool=25")
-    # Note that as of Rails 4.1 the `establish_connection` method requires
-    # the database_url be passed in as an argument. Like this:
-    # ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-  end
-
+  
   schedule_file = "config/schedule.yml"
 
   if File.exists?(schedule_file) && Sidekiq.server?
