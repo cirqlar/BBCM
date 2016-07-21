c4a8d21fee00aa2ad4f9ed3e59ab7411370c27da
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2016-07-21 07:19:17 +0100

    sde

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,10 +2,9 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://localhost:6379/0' }
-  database_url = '//localhost:6379'
+  database_url = '//localhost:6379/0'
   if database_url
-    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
-    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+    ActiveRecord::Base.establish_connection("#{database_url}?pool=25")
     # Note that as of Rails 4.1 the `establish_connection` method requires
     # the database_url be passed in as an argument. Like this:
     # ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
