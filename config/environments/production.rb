d9dd4b5d524d95b276127597ad53f8818a56b38d
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2016-07-21 06:18:35 +0100

    nmdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching =
 
   config.action_mailer.delivery_method = :smtp
-  host ENV['HOST']
+  host = ENV['HOST']
   config.action_mailer.default_url_options = { host: host }
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
