5cf04cbad4954543d815afff20a514066b7458ed
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2016-07-21 07:15:29 +0100

    dd

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,14 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://localhost:6379/0' }
+  database_url = '//localhost:6379'
+  if database_url
+    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
+    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+    # Note that as of Rails 4.1 the `establish_connection` method requires
+    # the database_url be passed in as an argument. Like this:
+    # ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+  end
 
   schedule_file = "config/schedule.yml"
 
