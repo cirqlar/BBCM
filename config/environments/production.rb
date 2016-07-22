7b64a7bc2548a2da19d22e2eae2600569eb10307
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2016-07-22 23:07:55 +0100

    LOL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,12 +20,11 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
-    :port                 => 465,
-    :domain               => "gmail.com",
+    :port                 => 587,
     :user_name            => ENV['GMAIL_USER'],
     :password             => ENV['GMAIL_PWD'],
-    :authentication       => "login",
-    :tls => true
+    :authentication       => "plain",
+    :enable_starttls_auto => true
   }
 
 
