e997b2d3a9aaba276f0a6180aac00c4af5fda388
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2016-07-23 22:49:26 +0100

    Add plupload gem and move all swf/xps? to public/deps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require plupload
 //= require jquery
 //= require mediaelement
 //= require jquery_ujs
