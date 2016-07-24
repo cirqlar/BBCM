ff18e5085e65e3122ec2897b236cd13ce790032e
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2016-07-24 23:22:35 +0100

    Remove plupload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require plupload
 //= require jquery
 //= require mediaelement
 //= require jquery_ujs
