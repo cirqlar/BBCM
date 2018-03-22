57b99e6bd1c5f47357a1c44b5f50d93da9cf9ce3
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2018-03-22 23:17:44 +0100

    Add logic to audio model

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,2 +1,12 @@
 class Audio < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
+
+  mount_uploader :poster, ImageUploader
+  mount_uploader :audio, AudioUploader
+
+  store_in_background :audio
+  acts_as_taggable
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :audio, presence: true
 end
